d798a08aceb9de27637d9d5046d25c8ca1fd8e67
randomfan022 <112581442+randomfan022@users.noreply.github.com>
2023-07-06 21:50:03 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,44 @@
-package com.example.smsprog;
+package com.example.prog5;
+import static android.content.Intent.ACTION_DIAL;
+import static android.content.Intent.ACTION_SEND;
 import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
- private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
- Button sendBtn;
- Button btnSendEmail;
- Button btnPhone;
- String phoneNo;
- String message;
+ Button sms,phone,mail;
+ @SuppressLint("MissingInflatedId")
  @Override
  protected void onCreate(Bundle savedInstanceState) {
  super.onCreate(savedInstanceState);
  setContentView(R.layout.activity_main);
- sendBtn = (Button) findViewById(R.id.btnSendSMS);
- btnSendEmail = (Button) findViewById(R.id.btnSendEmail);
- btnPhone = (Button) findViewById(R.id.btnDialPhone);
- sendBtn.setOnClickListener(new View.OnClickListener() {
- public void onClick(View view) {
- sendSMSMessage();
- }
- });
- btnSendEmail.setOnClickListener(new View.OnClickListener() {
+ sms=findViewById(R.id.sms);
+ phone=findViewById(R.id.call);
+ mail=findViewById(R.id.mail);
+ sms.setOnClickListener(new View.OnClickListener() {
  @Override
  public void onClick(View v) {
- sendEmail();
+ Intent in = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","7892094543",null));
+ startActivity(in);
  }
  });
- btnPhone.setOnClickListener(new View.OnClickListener() {
+ phone.setOnClickListener(new View.OnClickListener() {
  @Override
  public void onClick(View v) {
- PhoneDial();
+ Intent in = new Intent(ACTION_DIAL);
+ startActivity(in);
+ }
+ });
+ mail.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View v) {
+ Intent in = new Intent(ACTION_SEND);
+ in.setType("text/plain");
+ startActivity(in);
  }
  });
  }
- protected void sendEmail() {
- Intent emailIntent = new Intent(Intent.ACTION_SEND);
- emailIntent.setData(Uri.parse("mailto:"));
- emailIntent.setType("text/plain");
- emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ba.mohan@gmail.com"});
- emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject Test");
- emailIntent.putExtra(Intent.EXTRA_TEXT, "Message Body Test");
- startActivity(emailIntent);
- }
- protected void sendSMSMessage() {
- Intent sendIntent = new Intent(Intent.ACTION_VIEW);
- sendIntent.putExtra("sms_body", "default content");
- sendIntent.setType("vnd.android-dir/mms-sms");
- startActivity(sendIntent);
- Toast.makeText(getApplicationContext(), "SMS sent.",
- Toast.LENGTH_LONG).show();
- } protected void PhoneDial() {
- Intent intent = new Intent(Intent.ACTION_DIAL);
- startActivity(intent); }
 }
